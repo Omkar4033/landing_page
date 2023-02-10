d0efb0d78b4e64f8538693262712c9453d3fa752
Omkar4033 <91587106+Omkar4033@users.noreply.github.com>
2023-02-10 20:56:50 +0530

    corrected contact warning

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
         <h3>Send me Mail</h3>
         <div className='contact-input'>
             <input type='email' placeholder="example@gmail.com"/>
-            <a href='#'>Contact</a>
+            <a href='/'>Contact</a>
         </div>
     </div>
   )
